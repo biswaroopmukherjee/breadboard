9d9874bdc306ce93e1f0b1b0ada53c33317f2e35
Biswaroop Mukherjee <roop@mit.edu>
2020-02-19 17:41:55 -0500

    Updated django-cors-headers
    works for neptune and localhost

--- breadboard/settings.py
+++ breadboard/settings.py
@@ -45,9 +45,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    'localhost:3000/'
-)
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'https://neptune.fermi3.com'
+]
+# )
 
 ROOT_URLCONF = 'breadboard.urls'
 
